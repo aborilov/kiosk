f343460c8feeaabada6148eb22a05ba9dc1651b5
ggevorg <gev.gevorkyan@gmail.com>
2016-05-22 22:11:27 +0400

    Add changer_fsm and validator_fsm classes to project

--- kiosk/kiosk.py
+++ kiosk/kiosk.py
@@ -19,6 +19,9 @@ from serial import EIGHTBITS
 
 from pymdb.protocol.mdb import MDB
 from pymdb.device.changer import Changer, COINT_ROUTING
+from pymdb.device.bill_validator import BillValidator
+
+from ..fsm.changer_fsm import ChangerWrapper, ChangerFSM
 
 plugin.install_plugin(plugin.TwistedDispatchPlugin())
 
@@ -150,7 +153,62 @@ class Kiosk2(Machine):
         self.summ = 0
 
 
-class RUChanger(Changer):
+# class RUChanger(Changer):
+# 
+#     COINS = {
+#         0: 1,
+#         1: 2,
+#         2: 5,
+#         4: 10
+#     }
+# 
+#     def __init__(self, proto):
+#         super(RUChanger, self).__init__(proto)
+# 
+#     def start_accept(self):
+#         return self.coin_type(coins='\xFF\xFF')
+# 
+#     def stop_accept(self):
+#         return self.coin_type(coins='\x00\x00')
+# 
+#     def deposited(self, coin, routing=1, in_tube=None):
+#         logger.debug(
+#             "Coin deposited({}): {}".format(
+#                 COINT_ROUTING[routing], self.COINS[coin]))
+#         if routing == 1:
+#             amount = self.COINS[coin]
+#             dispatcher.send_minimal(
+#                 sender=self, signal='coin_in', amount=amount)
+# 
+#     def dispense_all(self):
+# 	#logger.debug("coin_count:  0x%0.2X" % self.coin_count[2])
+#         self.dispense(coin=0, count=10)
+#         self.dispense(coin=1, count=10)
+#         self.dispense(coin=2, count=10)
+#         self.dispense(coin=4, count=10)
+
+class RUChangerFSM(ChangerFSM):
+
+    def __init__(self, changer, amount=30):
+        super(RUChangerFSM, self).__init__(changer)
+        self.amount = amount
+        
+    def check_coin(self, amount):
+        accepted_amount = self._accepted_amount
+        need_amount = self.amount
+        if accepted_amount >= need_amount:
+            self.invalid_coin(amount)
+        elif accepted_amount + amount >= need_amount:
+            self.valid_coin(amount)
+            self.stop_accept()
+            self.dispense_amount(accepted_amount+amount-need_amount)
+        elif not self.can_dispense_amount(accepted_amount + amount):
+            self.invalid_coin(amount)
+        else:
+            self.valid_coin(amount)
+
+
+class RUChanger(ChangerWrapper):
 
     COINS = {
         0: 1,
@@ -160,41 +218,76 @@ class RUChanger(Changer):
     }
 
     def __init__(self, proto):
-        super(RUChanger, self).__init__(proto)
+        super(RUChanger, self).__init__(proto, COINS)
+
+    def dispense_all(self):
+    #logger.debug("coin_count:  0x%0.2X" % self.coin_count[2])
+        self.dispense(coin=0, count=10)
+        self.dispense(coin=1, count=10)
+        self.dispense(coin=2, count=10)
+        self.dispense(coin=4, count=10)
+
+
+class RUBillValidator(BillValidator):
+
+    def __init__(self, proto):
+        super(RUBillValidator, self).__init__(proto)
 
     def start_accept(self):
-        return self.coin_type(coins='\xFF\xFF')
+        return self.bill_type(bills='\xFF\xFF\xFF\xFF')
 
     def stop_accept(self):
-        return self.coin_type(coins='\x00\x00')
+        return self.bill_type(bills='\x00\x00\x00\x00')
 
-    def deposited(self, coin, routing=1, in_tube=None):
-        logger.debug(
-            "Coin deposited({}): {}".format(
-                COINT_ROUTING[routing], self.COINS[coin]))
-        if routing == 1:
-            amount = self.COINS[coin]
-            dispatcher.send_minimal(
-                sender=self, signal='coin_in', amount=amount)
 
 
 if __name__ == '__main__':
     proto = MDB()
     SerialPort(
         #  proto, '/dev/ttyUSB0', reactor,
-        proto, '/dev/ttyUSB0', reactor,
+        proto, '/dev/ttyMDB', reactor,
         baudrate='38400', parity=PARITY_NONE,
         bytesize=EIGHTBITS, stopbits=STOPBITS_ONE)
     changer = RUChanger(proto)
-    kiosk = Kiosk2(changer)
-    reactor.callLater(0, proto.mdb_init)
-    reactor.callLater(1, changer.reset)
-    reactor.callLater(2, changer.start_polling)
-    reactor.callLater(3, changer.dispense, coin=0, count=2)
+    changerFsm = RUChangerFSM(changer=changer)
+        
+    #validator = RUBillValidator(proto)
+    #kiosk = Kiosk2(changer)
+    
+    #reactor.callLater(0, proto.mdb_init)
+    
+    reactor.callLater(0, changerFsm.start)
+    reactor.callLater(5, changerFsm.start_accept)
+    #reactor.callLater(3, changerFsm.dispense_amount, 100)
+    #reactor.callLater(10, changerFsm.stop)
+
+    #reactor.callLater(1, changer.reset)
+    #reactor.callLater(2, changer.start_polling)
+    #reactor.callLater(3, changer.dispense, coin=0, count=5)
+    #reactor.callLater(4, changer.dispense, coin=1, count=5)
+    #reactor.callLater(5, changer.dispense, coin=2, count=5)
+    #reactor.callLater(6, changer.dispense, coin=4, count=5)
+    #reactor.callLater(3, changer.define_coin_count)
+    #reactor.callLater(4, changer.dispense_all)
+    #reactor.callLater(10, changer.start_accept)
+
+    #reactor.callLater(1, validator.reset)
+    #reactor.callLater(2, validator.start_polling)
+    #reactor.callLater(3, validator.escrow)
+    #reactor.callLater(4, validator.start_accept)
+    #reactor.callLater(12, validator.stop_accept)
+    #reactor.callLater(13, validator.escrow)
+
+    #reactor.callLater(10, validator.stop_accept)
+
+
+
     #  reactor.callLater(1, kiosk.start)
     #  kiosk = Kiosk(proto)
     #  reactor.callLater(0, kiosk.loop)
     #  reactor.callLater(3, kiosk.sell, 15)
     #  reactor.callLater(15, kiosk.stop_changer)
     #  ckkklogger.debug("run reactor")
+
+
     reactor.run()
